52222752e4d0b914586756480f40ba47cafce3e6
Jade Allen Cook <jadeallencook@MacBook-Pro-3.local>
2018-03-12 23:00:10 -0600

    🕹 Added answer functionality

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 (function() {
-    console.log(true);
+    function answer() {
+        // reset answers
+        document.getElementsByClassName('answer-bubble')[0].innerHTML = '';
+        document.getElementsByClassName('answer-bubble')[1].innerHTML = '';
+        // create fill element
+        var fill = document.createElement('div');
+        fill.classList.add('bubble-fill');
+        fill.classList.add('animated');
+        fill.classList.add('fadeIn');
+        this.appendChild(fill);
+    }
+    // add answer functionality to buttons
+    document.getElementsByClassName('answer-bubble')[0].onclick = answer;
+    document.getElementsByClassName('answer-bubble')[1].onclick = answer;
 })();
